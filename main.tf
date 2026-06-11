f4238c78424ef22d9147af2b7353e1af10f3fce2
Awab Eltigani <awabeltigani1@example.com>
2026-06-11 08:54:01 -0400

    change the region

--- main.tf
+++ main.tf
@@ -58,8 +58,8 @@ resource "aws_security_group_rule" "blog_https_in"{
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_in"{
-  type = "ingress"
+resource "aws_security_group_rule" "blog_http_out"{
+  type = "engress"
   from_port = 0
   to_port = 0
   protocol = "-1"
